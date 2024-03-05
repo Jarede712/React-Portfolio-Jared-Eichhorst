10e09ce06637136e82d7550d34a7a354d73f5e0d
Jarede712 <jaredeic@yahoo.com>
2024-03-05 11:36:12 -0800

    finished resume component and added list of proficiencies

---
+++ React Portfolio/src/components/Resume.jsx
@@ -0,0 +1,22 @@
+const Resume = () => {
+  return (
+    <section>
+      <h2>Resume</h2>
+      <p>
+        Download my resume <a href="<link to resume when I make one>">here</a>.
+      </p>
+      <div>
+        <h3>Proficiencies</h3>
+        <ul>
+          <li>Javascript</li>
+          <li>HTML & CSS</li>
+          <li>React</li>
+          <li>MongoDB</li>
+          <li>SQL</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
