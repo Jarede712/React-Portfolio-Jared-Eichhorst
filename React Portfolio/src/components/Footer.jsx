f1f9f922bc6b677149a912904938346a7444a433
Jarede712 <jaredeic@yahoo.com>
2024-03-05 10:38:27 -0800

    added footer with links to socials and placeholder for resume

---
+++ React Portfolio/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div>
+      <a href="https://github.com/Jarede712">GitHub</a>
+      <a href="https://www.linkedin.com/in/jared-eichhorst-b5a8382b3/">
+        LinkedIn
+      </a>
+      <a href="https://replit.com/@jaredeic">Replit</a>
+      <a href="#">Stack Overflow</a>
+    </div>
+  );
+};
+
+export default Footer;
