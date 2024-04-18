32244442998615c27b69b2623a1ec37c85833d16
Jarede712 <jaredeic@yahoo.com>
2024-04-18 14:32:32 -0700

    added react-router-dom to dependencies, added requirements on contact form fields before user can submit, made submission redirect to home page

--- React Portfolio/src/components/Contact.jsx
+++ React Portfolio/src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Contact = () => {
   const [formState, setFormState] = useState({
@@ -8,17 +9,29 @@ const Contact = () => {
     message: '',
   });
 
+  const [touched, setTouched] = useState({
+    name: false,
+    email: false,
+    message: false,
+  });
+
+  const navigate = useNavigate();
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     const response = await axios.post(
       'https://jaredeichhorst-portfolio.netlify.app/.netlify/functions/sendmail',
       formState
     );
+    navigate('/');
   };
 
   const handleChange = (event) => {
     setFormState({ ...formState, [event.target.id]: event.target.value });
+    setTouched({ ...touched, [event.target.id]: true });
   };
+
+  const isFormValid = formState.name && formState.email && formState.message;
 
   return (
     <section className="contact-container">
@@ -33,6 +46,7 @@ const Contact = () => {
             onChange={handleChange}
             value={formState.name}
           />
+          {touched.name && !formState.name && <div>Required</div>}
         </div>
         <div>
           <label htmlFor="email">Email:</label>
@@ -43,6 +57,7 @@ const Contact = () => {
             onChange={handleChange}
             value={formState.email}
           />
+          {touched.email && !formState.email && <div>Required</div>}
         </div>
         <div>
           <label htmlFor="message">Message:</label>
@@ -52,8 +67,13 @@ const Contact = () => {
             onChange={handleChange}
             value={formState.message}
           />
+          {touched.message && !formState.message && <div>Required</div>}
         </div>
-        <button type="submit" className="contact-submit-button">
+        <button
+          type="submit"
+          className="contact-submit-button"
+          disabled={!isFormValid}
+        >
           Submit
         </button>
       </form>
