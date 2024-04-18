b29322d4f8cb1d5eac34bea18374db44dd59e524
Jarede712 <jaredeic@yahoo.com>
2024-04-18 16:11:07 -0700

    fixed typo in about me section

--- React Portfolio/src/components/AboutMe.jsx
+++ React Portfolio/src/components/AboutMe.jsx
@@ -14,16 +14,16 @@ const AboutMe = () => {
           My name is Jared Eichhorst, I was born and rasied in San Diego and
           have recently found an interest in web development and computer
           science. After I graduated college, I struggled to figure out what was
-          next, I initially statrted working doing CAD drawings for a traffic
-          control company, but I knew that was not going to be a job I wanted to
-          stick with forever. Then I learned about web development, I did some
-          research on how to start a career in this field and found a bootcamp
-          that lined up well with my schedule. Now I am looking to pursue it as
-          a carrer. Web development for me is a stepping towards other tech
-          related jobs. Right now my focus is on that fully, but I have been
-          looking at other areas such as cybersecurity and game development. I
-          have always been interested in creating and this has been a great
-          outlet for me to express that.
+          next, I was initially working doing CAD drawings for a traffic control
+          company called Hudson Safe-T-Lite, but I knew that was not going to be
+          a job I wanted to stick with forever. Then I learned about web
+          development, I did some research on how to start a career in this
+          field and found a bootcamp that lined up well with my schedule. Now I
+          am looking to pursue it as a carrer. Web development for me is a
+          stepping towards other tech related jobs. Right now my focus is on
+          that fully, but I have been looking at other areas such as
+          cybersecurity and game development. I have always been interested in
+          creating and this has been a great outlet for me to express that.
         </p>
       </div>
     </section>
