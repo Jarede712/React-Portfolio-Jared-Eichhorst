a7786c7336f87e6d205ff053c8d6dce25709bc0c
Jarede712 <jaredeic@yahoo.com>
2024-04-18 13:41:27 -0700

    added endpoint for testing with netlify deploy, set up nodemailer with google cloud

--- React Portfolio/src/netlify/functions/sendmail.js
+++ React Portfolio/src/netlify/functions/sendmail.js
@@ -7,8 +7,12 @@ exports.handler = async function (event, context) {
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS,
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
     },
   });
 
