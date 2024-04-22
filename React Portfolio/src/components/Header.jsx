f625b9f49eab6f0ed0889e4b062421ea9dfa3742
Jarede712 <jaredeic@yahoo.com>
2024-04-22 13:32:19 -0700

    added animation to header and nav elements

--- React Portfolio/src/components/Header.jsx
+++ React Portfolio/src/components/Header.jsx
@@ -4,37 +4,47 @@ import wellfoundLogo from '../assets/wellfoundlogo.png';
 
 const Socials = ({ setCurrentPage }) => {
   return (
-    <IconContext.Provider value={{ className: 'social-icons' }}>
-      <div className="logo-container">
-        <a
-          href="https://github.com/jarede712"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="GitHub"
-        >
+    <div className="logo-container">
+      <a
+        href="https://github.com/jarede712"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="GitHub"
+      >
+        <IconContext.Provider value={{ className: 'social-icons github-icon' }}>
           <FaGithub />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/jared-eichhorst-b5a8382b3/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="LinkedIn"
+        </IconContext.Provider>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/jared-eichhorst-b5a8382b3/"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="LinkedIn"
+      >
+        <IconContext.Provider
+          value={{ className: 'social-icons linkedin-icon' }}
         >
           <FaLinkedin />
-        </a>
-        <a href="#" onClick={() => setCurrentPage('Contact')} title="Contact">
+        </IconContext.Provider>
+      </a>
+      <a href="#" onClick={() => setCurrentPage('Contact')} title="Contact">
+        <IconContext.Provider value={{ className: 'social-icons email-icon' }}>
           <FaEnvelope />
-        </a>
-        <a
-          href="link-to-wellfound"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Wellfound"
-        >
-          <img src={wellfoundLogo} alt="Wellfound" className="wellfound-logo" />
-        </a>
-      </div>
-    </IconContext.Provider>
+        </IconContext.Provider>
+      </a>
+      <a
+        href="link-to-wellfound"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Wellfound"
+      >
+        <img
+          src={wellfoundLogo}
+          alt="Wellfound"
+          className="social-icons wellfound-logo"
+        />
+      </a>
+    </div>
   );
 };
 
