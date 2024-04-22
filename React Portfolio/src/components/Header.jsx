ea5c1fdc3dcfffe1e09da28ba070e55b07a37be2
Jarede712 <jaredeic@yahoo.com>
2024-04-22 13:04:05 -0700

    Started adjusting header, added logos for socials and linked them to the right places

--- React Portfolio/src/components/Header.jsx
+++ React Portfolio/src/components/Header.jsx
@@ -1,16 +1,48 @@
-import Navigation from './Navigation';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
+import wellfoundLogo from '../assets/wellfoundlogo.png';
 
-const Header = ({ currentPage, setCurrentPage }) => {
-  const pages = ['About Me', 'Portfolio', 'Contact', 'Resume'];
+const Socials = ({ setCurrentPage }) => {
+  return (
+    <IconContext.Provider value={{ className: 'social-icons' }}>
+      <div className="logo-container">
+        <a
+          href="https://github.com/jarede712"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jared-eichhorst-b5a8382b3/"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a href="#" onClick={() => setCurrentPage('Contact')} title="Contact">
+          <FaEnvelope />
+        </a>
+        <a
+          href="link-to-wellfound"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Wellfound"
+        >
+          <img src={wellfoundLogo} alt="Wellfound" className="wellfound-logo" />
+        </a>
+      </div>
+    </IconContext.Provider>
+  );
+};
 
+const Header = ({ setCurrentPage }) => {
   return (
     <header className="header-style">
       <h1>Jared Eichhorst</h1>
-      <Navigation
-        pages={pages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+      <Socials setCurrentPage={setCurrentPage} />
     </header>
   );
 };
