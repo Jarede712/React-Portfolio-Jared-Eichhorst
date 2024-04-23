c4444509e99b3c6de99c098f5c067e51d07a7080
Jarede712 <jaredeic@yahoo.com>
2024-04-23 16:53:40 -0700

    started working on contact form

--- React Portfolio/src/components/Contact.jsx
+++ React Portfolio/src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     const { name, email, message } = formState;
-    const formattedMessage = `Name: ${name} - Email: ${email} - Message ${message}`;
+    const formattedMessage = `Name: ${name} - Email: ${email} - Message: ${message}`;
     const response = await axios.post(
       'https://jaredeichhorst-portfolio.netlify.app/.netlify/functions/sendmail',
       { message: formattedMessage }
@@ -38,51 +38,62 @@ const Contact = () => {
 
   return (
     <section className="contact-container">
-      <h2 className="contact-title">Contact</h2>
-      <button className="back-button" onClick={handleBack}>
-        Back
-      </button>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            required
-            onChange={handleChange}
-            value={formState.name}
-          />
-          {!formState.name && touched.name && <div>Required</div>}
+      <div className="content-container">
+        <div className="header-container"></div>
+        <div className="form-container">
+          <form onSubmit={handleSubmit}>
+            <h2 className="contact-title">Contact</h2>
+            <button className="back-button" onClick={handleBack}>
+              Back
+            </button>
+            <div>
+              <label htmlFor="name"></label>
+              <input
+                type="text"
+                id="name"
+                required
+                onChange={handleChange}
+                value={formState.name}
+                placeholder="Name"
+                className="contact-name-box"
+              />
+              {!formState.name && touched.name && <div>Required</div>}
+            </div>
+            <div>
+              <label htmlFor="email"></label>
+              <input
+                type="email"
+                id="email"
+                required
+                onChange={handleChange}
+                value={formState.email}
+                placeholder="Email"
+                className="contact-email-box"
+              />
+              {!formState.email && touched.email && <div>Required</div>}
+            </div>
+            <div>
+              <label htmlFor="message"></label>
+              <textarea
+                id="message"
+                required
+                onChange={handleChange}
+                value={formState.message}
+                placeholder="Message"
+                className="contact-message-box"
+              />
+              {!formState.message && touched.message && <div>Required</div>}
+            </div>
+            <button
+              type="submit"
+              className="contact-submit-button"
+              disabled={!isFormValid}
+            >
+              Submit
+            </button>
+          </form>
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            required
-            onChange={handleChange}
-            value={formState.email}
-          />
-          {!formState.email && touched.email && <div>Required</div>}
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            required
-            onChange={handleChange}
-            value={formState.message}
-          />
-          {!formState.message && touched.message && <div>Required</div>}
-        </div>
-        <button
-          type="submit"
-          className="contact-submit-button"
-          disabled={!isFormValid}
-        >
-          Submit
-        </button>
-      </form>
+      </div>
     </section>
   );
 };
